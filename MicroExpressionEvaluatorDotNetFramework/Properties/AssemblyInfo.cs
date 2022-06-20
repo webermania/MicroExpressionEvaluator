8895a5e8774caf904697864ff07686700201c4e3
Christopher Weber <c.weber@deliberate-gmbh.de>
2022-06-20 21:05:53 +0200

    description

--- MicroExpressionEvaluatorDotNetFramework/Properties/AssemblyInfo.cs
+++ MicroExpressionEvaluatorDotNetFramework/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MicroExpressionEvaluatorDotNetFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interpret and evaluate logic rule expressions represented as string (very quickly) and return evaluation success or throw Exception with clear Error message.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Webermania")]
 [assembly: AssemblyProduct("MicroExpressionEvaluatorDotNetFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
